88c5f1a07d757b69d0587c93ab0633dfc2cee36a
Eclipse <bigbossmelihcetin@gmail.com>
2026-06-10 12:35:55 +0300

    banner.svg

--- pwrinit/ui/banner.py
+++ pwrinit/ui/banner.py
@@ -1,9 +1,16 @@
-# Terminal Renk Kodları
-BLUE = "\033[94m"
-CYAN = "\033[96m"
-MAGENTA = "\033[95m"
-RESET = "\033[0m"
-
-print(CYAN + "██▓▓▓▒▒░░░░░▒▓ ██▓▓▓       ░░░▒▓ ██▓▓▓▒▒░░░░░▒▓█▓▓▒▒────░░▒▓▓ █▓▓▒▒       ░░▒▓▓ █▓▓▒▒────░░▒▓▓" + RESET)
-print(MAGENTA + "▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄  ▄▄▄  ▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄▄▄ ▓▓▓▓▓────────┘ ▓▓▓▓▓  ▓▓▓  ▓▓▓▓▓ ▓▓▓▓▓────▓▓▓▓▓" + RESET)
-print(BLUE + "▒▒▒▒▒          ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒    ▒▒▒▒▒└───┘          └───────────────┘ └───┘    └───┘" + RESET)
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 850 100" width="100%" height="100%">
+  <defs>
+    <linearGradient id="cyberGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+      <stop offset="0%" stop-color="#00f2fe" />   <stop offset="50%" stop-color="#4facfe" />  <stop offset="100%" stop-color="#f355da" /> </linearGradient>
+  </defs>
+  
+  <text x="10" y="30" font-family="monospace" font-size="11" font-weight="bold" fill="url(#cyberGradient)" xml:space="preserve">
+██▓▓▓▒▒░░░░░▒▓ ██▓▓▓       ░░░▒▓ ██▓▓▓▒▒░░░░░▒▓█▓▓▒▒────░░▒▓▓ █▓▓▒▒       ░░▒▓▓ █▓▓▒▒────░░▒▓▓
+  </text>
+  <text x="10" y="50" font-family="monospace" font-size="11" font-weight="bold" fill="url(#cyberGradient)" xml:space="preserve">
+▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ▄▄▄▄▄  ▄▄▄  ▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄▄▄▄▄ ▓▓▓▓▓────────┘ ▓▓▓▓▓  ▓▓▓  ▓▓▓▓▓ ▓▓▓▓▓────▓▓▓▓▓
+  </text>
+  <text x="10" y="70" font-family="monospace" font-size="11" font-weight="bold" fill="url(#cyberGradient)" xml:space="preserve">
+▒▒▒▒▒          ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒    ▒▒▒▒▒└───┘          └───────────────┘ └───┘    └───┘
+  </text>
+</svg>
